6a840af1b09436e74e60b2cfa784b331772c3ccb
kangjf_lv_juan <551730672@qq.com>
2016-05-31 23:33:03 +0800

    增加登录页面

--- blog/controllers/login.go
+++ blog/controllers/login.go
@@ -8,14 +8,28 @@ type LoginController struct {
 	beego.Controller
 }
 
-//实现 controller的登录方法
-//@router /login
-func (this *LoginController) Login() {
-	//登录页面不设置
-	//this.Data["xsrfdata"] = this.XSRFFormHTML()
-	this.Data["title"] = "登录页面"
+func (a *LoginController) Prepare() {
+	a.EnableXSRF = false
+}
 
+//@router /login页面
+func (this *LoginController) In() {
+	//登录页面不设置
+	this.Data["xsrfdata"] = this.XSRFFormHTML()
+	this.Data["title"] = "登录页面"
 	//跳转登录页面
+
 	this.TplName = "login.html"
 
+}
+
+func (this *LoginController) Login() {
+	user_name := this.Input().Get("user_name")
+	password := this.Input().Get("pwd")
+	//email := this.Input().Get("email")
+
+	//设置cookie
+	maxAge := 0
+	this.Ctx.SetCookie("user_name", user_name, maxAge, "/")
+	this.Ctx.SetCookie("user_name", password, maxAge, "/")
 }
